1c25794218de0930e257bd26e36aca4140c16028
ReemAlshathly <reem250139@gmail.com>
2022-03-28 08:40:49 +0300

    initial the project files

---
+++ app/controllers/home.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'controller.php';
+class Home extends Controller{
+
+    function __construct()
+    {
+        echo "<h1>inside home controller constuctor</h1>";
+    }
+
+    function index(){
+
+        echo "<h1>index of home</h1>";
+
+    }
+    function library()
+    {
+        $this->view('index');
+    }
+}
+?>
